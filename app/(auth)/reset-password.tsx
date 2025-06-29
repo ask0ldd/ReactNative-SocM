0e759862dda95fb61aa168b66fd87a0498b4a812
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-06-29 17:13:30 +0200

    *add* front / auth reset password screen basis

---
+++ app/(auth)/reset-password.tsx
@@ -0,0 +1,24 @@
+import { KeyboardAvoidingView, StyleSheet } from 'react-native';
+
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { ScrollView } from 'react-native-gesture-handler';
+
+export default function ResetPasswordScreen() {
+  return (
+    <KeyboardAvoidingView>
+        <ScrollView>
+            <ThemedView style={styles.titleContainer}>
+                <ThemedText type="title">Explore</ThemedText>
+            </ThemedView>
+        </ScrollView>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    gap: 8,
+  },
+});
