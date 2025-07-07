9683bdb1b7bdf1a79d9f1d628f85dba47a919bf1
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-07-07 20:43:35 +0200

    *edit* front / login screen layout + figma header pic

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,4 +1,4 @@
-import { KeyboardAvoidingView, Platform, ScrollView, StyleSheet } from 'react-native';
+import { Image, KeyboardAvoidingView, Platform, ScrollView, StyleSheet, useColorScheme } from 'react-native';
 
 import CustomButton from '@/components/CustomButton';
 import CustomFormInput from '@/components/CustomFormInput';
@@ -12,6 +12,8 @@ export default function LoginScreen() {
 
     const [focus, setFocus] = useState<"email" | "password" | null>(null)
 
+    const colorScheme = useColorScheme()
+
     return (
         <KeyboardAvoidingView 
             style={{ flex: 1 }}
@@ -22,37 +24,51 @@ export default function LoginScreen() {
                 contentContainerStyle={styles.scrollView}
                 keyboardShouldPersistTaps="handled"
             >
+                <Image 
+                  source={colorScheme === "dark" ? require('@/assets/images/figma.png') : require('@/assets/images/figma.png')} 
+                  resizeMode="cover" 
+                  style={{width: '100%', height:325}}
+                />
                 <ThemedView style={styles.mainContainer}>
-                    <ThemedText>Login</ThemedText>
-                    <CustomFormInput
-                        style={{fontSize:16, fontFamily:'Jost_400Regular'}}
-                        active={focus === "email"}
-                        placeholder={focus === "email" ? "" : "Email"}
-                        value={form.email}
-                        onChangeText={(email: string) => setForm(prevForm => ({...prevForm, email}))}
-                        keyboardType="email-address"
-                        autoCapitalize="none"
-                        containerStyle={{...styles.input, columnGap : 10}}
-                        accessibilityLabel="Champ email"
-                        placeholderTextColor={'#8E8E93'}
-                        onFocus={() => setFocus("email")}
-                        onBlur={() => setFocus(null)}
-                    />
-                    <CustomFormInput
-                        style={{fontSize:16, fontFamily:'Jost_400Regular'}}
-                        active={focus === "password"}
-                        placeholder={focus === "password" ? "" : "Password"}
-                        value={form.password}
-                        onChangeText={(password: string) => setForm(prevForm => ({...prevForm, password}))}
-                        keyboardType="default"
-                        autoCapitalize="none"
-                        containerStyle={{...styles.input, columnGap : 10, marginTop:15}}
-                        accessibilityLabel="Champ password"
-                        placeholderTextColor={'#8E8E93'}
-                        onFocus={() => setFocus("password")}
-                        onBlur={() => setFocus(null)}
-                    />
-                    <CustomButton gradient={} title={'Login'} onPress={() => void 0}/>
+                    <ThemedView style={styles.form}>
+                        <ThemedText style={{fontFamily:'Jost_600SemiBold', fontSize:24, lineHeight:32}}>Login</ThemedText>
+                        <CustomFormInput
+                            style={{fontSize:16, fontFamily:'Jost_400Regular'}}
+                            active={focus === "email"}
+                            placeholder={focus === "email" ? "" : "Email"}
+                            value={form.email}
+                            onChangeText={(email: string) => setForm(prevForm => ({...prevForm, email}))}
+                            keyboardType="email-address"
+                            autoCapitalize="none"
+                            containerStyle={{...styles.input, columnGap : 10, marginTop:10}}
+                            accessibilityLabel="Champ email"
+                            placeholderTextColor={'#8E8E93'}
+                            onFocus={() => setFocus("email")}
+                            onBlur={() => setFocus(null)}
+                        />
+                        <CustomFormInput
+                            style={{fontSize:16, fontFamily:'Jost_400Regular'}}
+                            active={focus === "password"}
+                            placeholder={focus === "password" ? "" : "Password"}
+                            value={form.password}
+                            onChangeText={(password: string) => setForm(prevForm => ({...prevForm, password}))}
+                            keyboardType="default"
+                            autoCapitalize="none"
+                            containerStyle={{...styles.input, columnGap : 10, marginTop:20}}
+                            accessibilityLabel="Champ password"
+                            placeholderTextColor={'#8E8E93'}
+                            onFocus={() => setFocus("password")}
+                            onBlur={() => setFocus(null)}
+                        />
+                        <CustomButton gradient={{
+                            colors: colorScheme === "dark" ? ['#007AFF', '#007AFF'] : ['#007AFF', '#007AFF'],
+                            start: { x: 0, y: 0 },
+                            end: { x: 1, y: 0 }}} 
+                            title={'Login'} 
+                            onPress={() => void 0}
+                            styles={{button: styles.sendButton, text:{color : '#ffffff', fontSize:16, fontFamily:'Jost_600SemiBold'}}}
+                        />
+                    </ThemedView>
                 </ThemedView>
             </ScrollView>
         </KeyboardAvoidingView>
@@ -70,7 +86,8 @@ const styles = StyleSheet.create({
         width:'100%', 
         flex: 1,
         flexDirection:'column', 
-        padding: 20,
+        paddingHorizontal: 20,
+        paddingBottom:20,
         fontFamily:'Jost_400Regular',
     },
     input:{
@@ -80,14 +97,20 @@ const styles = StyleSheet.create({
         borderStyle: 'solid',
         paddingHorizontal : 15,
         borderRadius:8,
-        height:50,
+        height:55,
         color:'#323232',
         fontFamily:'Jost_400Regular',
         fontSize:16
     },
     form:{
         flexDirection:'column', 
-        marginTop:30,
+        marginTop:20,
         width:'100%'
     },
+    sendButton:{
+        marginTop:20, 
+        height:55,
+        shadowColor:'#007AFF',
+        elevation:4,
+    }
 });
