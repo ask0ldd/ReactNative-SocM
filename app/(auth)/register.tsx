6a469567d8c01d92e272aad156f15e11e682c111
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-07-09 19:27:34 +0200

    *add* front / implementation of the registration screen

--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -1,24 +1,173 @@
-import { KeyboardAvoidingView, StyleSheet } from 'react-native';
+import { KeyboardAvoidingView, Platform, ScrollView, StyleSheet, useColorScheme } from 'react-native';
 
+import CustomButton from '@/components/CustomButton';
+import CustomFormInput from '@/components/CustomFormInput';
 import { ThemedText } from '@/components/expo/ThemedText';
 import { ThemedView } from '@/components/expo/ThemedView';
-import { ScrollView } from 'react-native-gesture-handler';
+import { isNativeFirebaseError } from '@/guards/isNativeFirebaseError';
+import { useServicesContext } from '@/hooks/context/useServices';
+import registrationFormSchema from '@/zod/forms/registrationFormSchema';
+import { router } from 'expo-router';
+import { useState } from 'react';
+import { ZodError } from 'zod/v4';
+
+const defaultFormValue = {
+  email : "", 
+  password : "", 
+  passwordCheck : ""
+}
 
 export default function RegisterScreen() {
+  const {storageService, authService} = useServicesContext()
+  
+  const [form, setForm] = useState<{email : string, password : string, passwordCheck : string}>(defaultFormValue)
+  
+  const [focus, setFocus] = useState<"email" | "password" | "passwordCheck" | null>(null)
+  const [errorMessage, setErrorMessage] = useState<string>("")
+
+  const colorScheme = useColorScheme()
+
+  async function handleRegisterFormSubmit(){
+    setErrorMessage("")
+    try{
+        // form verification : throws an error if verification fails
+        registrationFormSchema.parse(form)
+        if(form.password !== form.passwordCheck) throw new Error("Erreur lors de la confirmation de votre mot de passe.")
+        // checks by default if the email is available
+        await authService.createUserWithEmailAndPassword(form.email, form.password)
+        router.push('/(onboarding)')
+    }catch(error : unknown){
+        const message = parseRegistrationFormError(error)
+        setErrorMessage(message)
+        console.error(message)
+    }
+  };
+
+
   return (
-    <KeyboardAvoidingView>
-        <ScrollView>
-            <ThemedView style={styles.titleContainer}>
-                <ThemedText type="title">Explore</ThemedText>
-            </ThemedView>
-        </ScrollView>
-    </KeyboardAvoidingView>
+      <KeyboardAvoidingView 
+          style={{ flex: 1 }}
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          keyboardVerticalOffset={0}
+      >
+          <ScrollView 
+              contentContainerStyle={styles.scrollView}
+              keyboardShouldPersistTaps="handled"
+          >
+              <ThemedView style={styles.mainContainer}>
+                  <ThemedView style={styles.form}>
+                      <ThemedText style={{fontFamily:'Jost_600SemiBold', fontSize:24, lineHeight:32}}>REGISTER</ThemedText>
+                      <CustomFormInput
+                          style={{fontSize:16, fontFamily:'Jost_400Regular'}}
+                          active={focus === "email"}
+                          placeholder={focus === "email" ? "" : "Email"}
+                          value={form.email}
+                          onChangeText={(email: string) => setForm(prevForm => ({...prevForm, email}))}
+                          keyboardType="email-address"
+                          autoCapitalize="none"
+                          containerStyle={{...styles.input, columnGap : 10, marginTop:10}}
+                          accessibilityLabel="Champ email"
+                          placeholderTextColor={'#8E8E93'}
+                          onFocus={() => setFocus("email")}
+                          onBlur={() => setFocus(null)}
+                      />
+                      <CustomFormInput
+                          style={{fontSize:16, fontFamily:'Jost_400Regular'}}
+                          active={focus === "password"}
+                          placeholder={focus === "password" ? "" : "Password"}
+                          value={form.password}
+                          onChangeText={(password: string) => setForm(prevForm => ({...prevForm, password}))}
+                          keyboardType="default"
+                          textContentType="password"
+                          secureTextEntry={true}
+                          autoCapitalize="none"
+                          containerStyle={{...styles.input, columnGap : 10, marginTop:20}}
+                          accessibilityLabel="Champ password"
+                          placeholderTextColor={'#8E8E93'}
+                          onFocus={() => setFocus("password")}
+                          onBlur={() => setFocus(null)}
+                      />
+                      <CustomFormInput
+                          style={{fontSize:16, fontFamily:'Jost_400Regular'}}
+                          active={focus === "passwordCheck"}
+                          placeholder={focus === "passwordCheck" ? "" : "Password Verification"}
+                          value={form.passwordCheck}
+                          onChangeText={(passwordCheck: string) => setForm(prevForm => ({...prevForm, passwordCheck}))}
+                          keyboardType="default"
+                          textContentType="password"
+                          secureTextEntry={true}
+                          autoCapitalize="none"
+                          containerStyle={{...styles.input, columnGap : 10, marginTop:20}}
+                          accessibilityLabel="Champ password"
+                          placeholderTextColor={'#8E8E93'}
+                          onFocus={() => setFocus("passwordCheck")}
+                          onBlur={() => setFocus(null)}
+                      />
+                      <CustomButton gradient={{
+                          colors: colorScheme === "dark" ? ['#007AFF', '#007AFF'] : ['#007AFF', '#007AFF'],
+                          start: { x: 0, y: 0 },
+                          end: { x: 1, y: 0 }}} 
+                          title={'Register'} 
+                          onPress={handleRegisterFormSubmit}
+                          styles={{button: styles.sendButton, text:{color : '#ffffff', fontSize:16, fontFamily:'Jost_600SemiBold'}}}
+                      />
+                  </ThemedView>
+              </ThemedView>
+          </ScrollView>
+      </KeyboardAvoidingView>
   );
 }
 
+function parseRegistrationFormError(error: unknown): string {
+    if (isNativeFirebaseError(error)) {
+        if (error.code === 'auth/email-already-in-use') return "Email indisponible.";
+        if (error.code === 'auth/invalid-credential') return "Identifiants inconnus.";
+        if (error.code === 'auth/invalid-email') return "Email inconnu.";
+        return error.message;
+    }
+    if (error instanceof ZodError) {
+        return JSON.parse(error.message)[0].message
+    }
+    return `${error}`;
+}
+
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
+  scrollView :{ 
+      flexGrow: 1, 
+      flexDirection:'column',
+      justifyContent: 'center', 
+      alignItems: 'center',
   },
+  mainContainer:{
+      width:'100%', 
+      flex: 1,
+      flexDirection:'column', 
+      paddingHorizontal: 20,
+      paddingBottom:20,
+      fontFamily:'Jost_400Regular',
+  },
+  input:{
+      backgroundColor:'#F2F2F7',
+      borderWidth: 1,
+      borderColor: '#D1D1D6',
+      borderStyle: 'solid',
+      paddingHorizontal : 15,
+      borderRadius:8,
+      height:55,
+      color:'#323232',
+      fontFamily:'Jost_400Regular',
+      fontSize:16,
+  },
+  form:{
+      flexDirection:'column', 
+      marginTop:20,
+      width:'100%',
+  },
+  sendButton:{
+      marginTop:20, 
+      height:55,
+      shadowColor:'#007AFF',
+      elevation:4,
+  }
 });
+  
