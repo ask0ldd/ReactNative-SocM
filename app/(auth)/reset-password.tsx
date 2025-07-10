bf4f7affd8f558796538763b41ddea77bcc80f5c
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-07-10 20:59:20 +0200

    *edit* front / reset password screen layout

--- app/(auth)/reset-password.tsx
+++ app/(auth)/reset-password.tsx
@@ -1,18 +1,49 @@
-import { KeyboardAvoidingView, StyleSheet } from 'react-native';
-
+import CustomButton from '@/components/CustomButton';
+import CustomFormInput from '@/components/CustomFormInput';
 import { ThemedText } from '@/components/expo/ThemedText';
 import { ThemedView } from '@/components/expo/ThemedView';
-import { ScrollView } from 'react-native-gesture-handler';
+import { KeyboardAvoidingView, Platform, ScrollView, StyleSheet } from 'react-native';
 
 export default function ResetPasswordScreen() {
   return (
-    <KeyboardAvoidingView>
-        <ScrollView>
-            <ThemedView style={styles.titleContainer}>
-                <ThemedText type="title">Explore</ThemedText>
-            </ThemedView>
-        </ScrollView>
-    </KeyboardAvoidingView>
+    <KeyboardAvoidingView 
+              style={{ flex: 1 }}
+              behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+              keyboardVerticalOffset={0}
+          >
+              <ScrollView 
+                  contentContainerStyle={styles.scrollView}
+                  keyboardShouldPersistTaps="handled"
+              >
+                  <ThemedView style={styles.mainContainer}>
+                      <ThemedView style={styles.form}>
+                          <ThemedText style={{fontFamily:'Jost_600SemiBold', fontSize:24, lineHeight:32}}>REGISTER</ThemedText>
+                          <CustomFormInput
+                              style={{fontSize:16, fontFamily:'Jost_400Regular'}}
+                              active={focus === "email"}
+                              placeholder={focus === "email" ? "" : "Email"}
+                              value={form.email}
+                              onChangeText={(email: string) => setForm(prevForm => ({...prevForm, email}))}
+                              keyboardType="email-address"
+                              autoCapitalize="none"
+                              containerStyle={{...styles.input, columnGap : 10, marginTop:10}}
+                              accessibilityLabel="Champ email"
+                              placeholderTextColor={'#8E8E93'}
+                              onFocus={() => setFocus("email")}
+                              onBlur={() => setFocus(null)}
+                          />
+                          <CustomButton gradient={{
+                              colors: colorScheme === "dark" ? ['#007AFF', '#007AFF'] : ['#007AFF', '#007AFF'],
+                              start: { x: 0, y: 0 },
+                              end: { x: 1, y: 0 }}} 
+                              title={'Register'} 
+                              onPress={handleRegisterFormSubmit}
+                              styles={{button: styles.sendButton, text:{color : '#ffffff', fontSize:16, fontFamily:'Jost_600SemiBold'}}}
+                          />
+                      </ThemedView>
+                  </ThemedView>
+              </ScrollView>
+          </KeyboardAvoidingView>
   );
 }
 
