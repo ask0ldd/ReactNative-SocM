e86f05edbdc77afa4e20400ef39dbe7897093f2e
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-07-10 21:03:35 +0200

    *edit* front / reset password screen logic

--- app/(auth)/reset-password.tsx
+++ app/(auth)/reset-password.tsx
@@ -2,9 +2,39 @@ import CustomButton from '@/components/CustomButton';
 import CustomFormInput from '@/components/CustomFormInput';
 import { ThemedText } from '@/components/expo/ThemedText';
 import { ThemedView } from '@/components/expo/ThemedView';
-import { KeyboardAvoidingView, Platform, ScrollView, StyleSheet } from 'react-native';
+import { isNativeFirebaseError } from '@/guards/isNativeFirebaseError';
+import { useServicesContext } from '@/hooks/context/useServices';
+import { router } from 'expo-router';
+import { useState } from 'react';
+import { KeyboardAvoidingView, Platform, ScrollView, StyleSheet, useColorScheme } from 'react-native';
+import z, { ZodError } from 'zod/v4';
 
 export default function ResetPasswordScreen() {
+  
+  const {storageService, authService} = useServicesContext()
+  
+  const [email, setEmail] = useState<string>("")
+  
+  const [focus, setFocus] = useState<"email" | "password" | "passwordCheck" | null>(null)
+  const [errorMessage, setErrorMessage] = useState<string>("")
+
+  const colorScheme = useColorScheme()
+
+  async function handleRegisterFormSubmit(){
+    setErrorMessage("")
+    try{
+        // form verification : throws an error if verification fails
+        z.email().parse(email)
+        // checks by default if the email is available
+        await authService.sendPasswordResetEmail(email)
+        router.push('/(onboarding)')
+    }catch(error : unknown){
+        const message = parseResetPasswordFormError(error)
+        setErrorMessage(message)
+        console.error(message)
+    }
+  };
+
   return (
     <KeyboardAvoidingView 
               style={{ flex: 1 }}
@@ -22,8 +52,8 @@ export default function ResetPasswordScreen() {
                               style={{fontSize:16, fontFamily:'Jost_400Regular'}}
                               active={focus === "email"}
                               placeholder={focus === "email" ? "" : "Email"}
-                              value={form.email}
-                              onChangeText={(email: string) => setForm(prevForm => ({...prevForm, email}))}
+                              value={email}
+                              onChangeText={setEmail}
                               keyboardType="email-address"
                               autoCapitalize="none"
                               containerStyle={{...styles.input, columnGap : 10, marginTop:10}}
@@ -45,6 +75,19 @@ export default function ResetPasswordScreen() {
               </ScrollView>
           </KeyboardAvoidingView>
   );
+}
+
+function parseResetPasswordFormError(error: unknown): string {
+    if (isNativeFirebaseError(error)) {
+        if (error.code === 'auth/email-already-in-use') return "Email indisponible.";
+        if (error.code === 'auth/invalid-credential') return "Identifiants inconnus.";
+        if (error.code === 'auth/invalid-email') return "Email inconnu.";
+        return error.message;
+    }
+    if (error instanceof ZodError) {
+        return JSON.parse(error.message)[0].message
+    }
+    return `${error}`;
 }
 
 const styles = StyleSheet.create({
